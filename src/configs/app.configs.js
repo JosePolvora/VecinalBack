1eff7c122ef127f1b3173574724f24589172eed1
Jose Oviedo <josepolvoraoviedo@gmail.com>
2025-08-04 19:33:31 -0300

    Configurar variables de entorno y conexión a BD

--- src/configs/app.configs.js
+++ src/configs/app.configs.js
@@ -1,10 +1,26 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize("dbcvecinal", "root", '', {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: "3306"
+// }
+// );
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize("dbcvecinal", "root", '', {
-    host: "localhost",
-    dialect: "mysql",
-    port: "3306"
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+    logging: false
+  }
 );
 
 module.exports = sequelize;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();  // <--- Aquí va, al principio
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
